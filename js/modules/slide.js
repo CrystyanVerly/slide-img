242a19501922a194fb6d538c31160998a85c8177
Crystyan Knapp Verly <crystyanknappverly@gmail.com>
2023-04-26 14:13:53 -0300

    add funcionalidade de movimentacao

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,21 +1,39 @@
 export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
-    this.slide = document.querySelectorAll(slide);
+    this.slide = document.querySelector(slide);
+    this.distance = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.distance.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+  }
+
+  updatePosition(clientX) {
+    this.distance.movement = (this.distance.startX - clientX) * 1.6;
+    return this.distance.finalPosition - this.distance.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+    this.distance.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onMove(event) {
-    event.preventDefault();
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
     event.preventDefault();
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.distance.finalPosition = this.distance.movePosition;
   }
 
   addEventSlide() {
